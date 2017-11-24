4e1375d181c09b75aee0f9c425fcb89d07d21fc1
Bavan <Bavan@161.23.162.67>
2017-11-24 18:48:16 +0000

    added inverse method to Matrix2x2 Matrices need to be converted to generic

--- src/com/qmul/matrix/Matrix2x2.java
+++ src/com/qmul/matrix/Matrix2x2.java
@@ -27,4 +27,20 @@ public class Matrix2x2 extends Matrix{
         det = this.get(0,0)*this.get(1,1) - this.get(0,1)*this.get(1,0);
         return det;
     }
+    
+    @Override
+    public Matrix2x2 inverse(){
+        int[][] invertedMatrix = new int[2][2];
+        int det = this.determinant();
+        if(det!=0){
+            invertedMatrix[0][0] = (1/det)*this.get(1, 1);
+            invertedMatrix[0][1] = (1/det)*(-1)*this.get(0,1);
+            invertedMatrix[1][0] = (1/det)*(-1)*this.get(1,0);
+            invertedMatrix[1][1] = (1/det)*this.get(0,0);
+        }
+        else{
+            throw new ArithmeticException("Inverse cant be calculated if the determinant is 0");
+        }
+        return new Matrix2x2(invertedMatrix);
+    }
 }
