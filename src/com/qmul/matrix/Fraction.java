0230eb6842908fa57d951211080f781e3541eb2a
Windows 10 <Windows 10@138.37.227.142>
2017-11-25 20:24:00 +0000

    Fraction class

---
+++ src/com/qmul/matrix/Fraction.java
@@ -0,0 +1,164 @@
+package com.qmul.matrix;
+
+/**
+ *
+ * @author Abdullah Hasan
+ */
+public class Fraction {
+    private int d,n;
+    
+    
+    public Fraction()
+    {
+        this.n = 0;
+        this.d = 1;
+        
+    }
+    
+    public Fraction(int d, int n)
+    {
+        if(d == 0)
+            throw new IllegalArgumentException("Denominator can't be zero");
+        this.d = d;
+        this.n = n;
+    }
+    
+    public Fraction(String s)
+    {
+        String[] in = s.split("/");
+        if(in.length != 3) throw new IllegalArgumentException("Invalid fraction string");
+        int a, b;
+        a = Integer.parseInt(in[2]);
+        b = Integer.parseInt(in[0]);
+        if(a == 0) throw new IllegalArgumentException("Denominator can't be zero");
+        this.d = a;
+        this.n = b;
+    }
+    
+    /**
+     * Add fraction
+     * @param A Fraction to add
+     * @return Fraction result of addition
+     */
+    public Fraction add(Fraction A)
+    {
+        int a = A.getDeno();
+        int b = A.getNumer();
+        return simplify(new Fraction((a * d),(a*n + d*b)));
+    }
+    
+    /**
+     * Subtracts fraction
+     * @param A Fraction to subtract
+     * @return Fraction result of subtraction
+     */
+    public Fraction subtract(Fraction A)
+    {
+        int a = A.getDeno();
+        int b = A.getNumer();
+        return simplify(new Fraction((a * d),(a*n - d*b)));
+    }
+    
+    /**
+     * Multiply fractions
+     * @param A Fraction to multiply
+     * @return Fraction result
+     */
+    public Fraction multiply(Fraction A)
+    {
+        int a = A.getDeno();
+        int b = A.getNumer();
+        return simplify(new Fraction(a*d,b*n));
+    }
+    
+    /**
+     * Divides by Fraction
+     * @param A Fraction to divide by
+     * @return Fraction result of division
+     */
+    public Fraction divide(Fraction A)
+    {
+        int a = A.getDeno();
+        int b = A.getNumer();
+        return simplify(new Fraction(a*n,b*d));
+    }
+    
+    
+    /**
+     * Simplifies a fraction
+     * @param A Fraction to simplify
+     * @return Simplified Fraction
+     */
+    public Fraction simplify(Fraction A)
+    {
+        int a = A.getDeno();
+        int b = A.getNumer();
+        int gcd = GCD(a,b);
+        return new Fraction((a/gcd),(b/gcd));
+    }
+    
+    /**
+     * Finds greatest common divisor of two integers using Euclidean algorithm
+     * @param a first integer 
+     * @param b second integer
+     * @return integer that is GCD of both arguments
+     */
+    private int GCD(int a, int b)
+    {
+        if(b == 0) return a;
+        return GCD(b, a%b);
+    }
+    
+    /**
+     * Converts fraction to string representation
+     * 
+     * @return String
+     */
+    @Override
+    public String toString()
+    {
+        return Integer.toString(this.n) + "/" + Integer.toString(this.d);
+    }
+    
+    /**
+     * Returns decimal form of fraction
+     * @return float
+     */
+    public float toDecimal()
+    {
+        return n/d;
+    }
+
+    /**
+     * Compares two fractions
+     * @param A Fraction to compare to
+     * @return integer. -1 if argument is larger, 0 if the same, 1 if argument is smaller
+     */
+    public int compareTo(Fraction A) {
+        int f = this.d * A.getNumer();
+        int s = this.n * A.getDeno();
+        if(f < s) return 1;
+        else if(f > s) return -1;
+        else return 0;
+    }
+    
+    /**
+     * Gets denominator
+     * @return int
+     */
+    public int getDeno()
+    {
+        return d;
+    }
+    
+    /**
+     * gets numerator
+     * @return int
+     */
+    public int getNumer()
+    {
+        return n;
+    }
+    
+    
+}
